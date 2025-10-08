95245e893a74fe105101d80b015378cbdacfed12
Kushal <97932413+kushalchaka@users.noreply.github.com>
2025-10-07 20:02:06 -0500

    Update gclient.c

--- gclient.c
+++ gclient.c
@@ -1,15 +1,3 @@
-/* make up unique pipename for client
- * send it to the server (public pipe)
- * get another unique pipename from the server
- * repeat
- *      read a line including starred word from the client pipe
- *  display that line to the user
- *  check whether game is over?
- *  get the user's guess letter
- *  send to the server using server pipe
- */
-
-// custom client for this specific server
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
